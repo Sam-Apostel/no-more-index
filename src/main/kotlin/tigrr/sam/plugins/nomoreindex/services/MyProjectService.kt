c644ff44c09af796470d7551ae7a32c28e20a639
sam@apostel.be <sam@apostel.be>
2020-08-14 07:23:41 +0200

    placed imports in lexographical order

--- src/main/kotlin/tigrr/sam/plugins/nomoreindex/services/MyProjectService.kt
+++ src/main/kotlin/tigrr/sam/plugins/nomoreindex/services/MyProjectService.kt
@@ -1,7 +1,7 @@
 package tigrr.sam.plugins.nomoreindex.services
 
-import tigrr.sam.plugins.nomoreindex.MyBundle
 import com.intellij.openapi.project.Project
+import tigrr.sam.plugins.nomoreindex.MyBundle
 
 class MyProjectService(project: Project) {
 
